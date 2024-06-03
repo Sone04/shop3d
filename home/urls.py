5a0a6b6702f51c001efef847a48bef1622b72e87
Sone04 <srdicnebojsa78@gmail.com>
2024-06-04 01:11:41 +0200

    ide sga

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,11 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.home, name="home"),
+    path('kontakt/', views.kontakt, name="kontakt"),
+    path('onama/', views.onama, name="onama"),
+    path('proizvodi/', views.proizvodi, name="proizvodi"),
+    path('tehnologija/', views.tehnologija, name="tehnologija"),
+    path('blog/', views.blog, name="blog"),
+    path('usluge/', views.usluge, name="usluge"),
 ] 
 
--- home/views.py
+++ home/views.py
@@ -5,3 +5,27 @@ from django.shortcuts import render
 def home (request):
 
     return render(request, 'home/home.html')
+
+def kontakt (request):
+
+    return render(request, 'home/kontakt.html')
+
+def onama (request):
+
+    return render(request, 'home/onama.html')
+
+def proizvodi (request):
+
+    return render(request, 'home/proizvodi.html')
+
+def tehnologija (request):
+
+    return render(request, 'home/tehnologija.html')
+
+def usluge (request):
+
+    return render(request, 'home/usluge.html')
+
+def blog (request):
+
+    return render(request, 'home/blog.html')
